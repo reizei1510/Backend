7f1ed47ecab495b3896799f01c51e7119bc25c35
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-20 00:44:13 +0300

    Update index.php

--- 4ex/index.php
+++ 4ex/index.php
@@ -2,6 +2,7 @@
 header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    
     if (!empty($_GET['save'])) {
         echo "<script type='text/javascript'>alert('Результаты сохранены.');</script>";
         setcookie('save_error', '', 1000000);
@@ -19,14 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $errors['save'] = empty($_COOKIE['save_error']) ? false : $_COOKIE['save_error'];
     
     $values = array();
-    $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
-    $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
-    $values['limbs'] = empty($_COOKIE['limbs_value']) ? '' : $_COOKIE['limbs_value'];
-    $values['gender'] = empty($_COOKIE['gender_value']) ? '' : $_COOKIE['gender_value'];
-    $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
-    $values['birthday'] = empty($_COOKIE['birthday_value']) ? '' : $_COOKIE['birthday_value'];
-    $values['superpowers'] = empty($_COOKIE['superpowers_value']) ? [] : explode(', ', $_COOKIE['superpowers_value']);
-    $values['contract'] = empty($_COOKIE['contract_value']) ? '' : $_COOKIE['contract_value'];
+    $values['name'] = $_COOKIE['name_value'];
+    $values['email'] = $_COOKIE['email_value'];
+    $values['limbs'] = $_COOKIE['limbs_value'];
+    $values['gender'] = $_COOKIE['gender_value'];
+    $values['biography'] = $_COOKIE['biography_value'];
+    $values['birthday'] = $_COOKIE['birthday_value'];
+    $values['superpowers'] = explode(', ', $_COOKIE['superpowers_value']);
+    $values['contract'] = $_COOKIE['contract_value'];
     
     include('form.php');
 }
@@ -35,84 +36,95 @@ else {
     if (empty($_POST['name'])) {
         setcookie('name_error', 'empty');
         $errors = TRUE;
+        setcookie('name_value', $_POST['name'], 0);
     }
     else if (!preg_match("/^[A-Z][a-z]+$/", $_POST['name'])){
         setcookie('name_error', 'incorrect');
         $errors = TRUE;
+        setcookie('name_value', $_POST['name'], 0);
     }
     else {
-        setcookie('name_value', $_POST['name'], time() + 30 * 24 * 60 * 60);
+        setcookie('name_value', $_POST['name'], time() + 365 * 24 * 60 * 60);
         setcookie('name_error', '', 100000);
     }
     
     if (empty($_POST['email'])) {
         setcookie('email_error', 'empty');
         $errors = TRUE;
+        setcookie('email_value', $_POST['email'], 0);
       }
     else if (!preg_match("/^[A-Za-z0-9][A-Za-z0-9_\-.]+@[A-Za-z0-9][A-Za-z0-9_\-.]+\.[A-Za-z]+$/",$_POST['email'])){
         setcookie('email_error', 'incorrect');
         $errors = TRUE;
+        setcookie('email_value', $_POST['email'], 0);
     } 
     else {
-        setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+        setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
         setcookie('email_error', '', 100000);
     }
 
     if (empty($_POST['birthday'])) {
         setcookie('birthday_error', 'empty');
         $errors = TRUE;
+        setcookie('birthday_value', $_POST['birthday'], 0);
     }
     else if ($_POST['birthday'] > date('Y-m-d')) {
         setcookie('birthday_error', 'incorrect');
         $errors = TRUE;
+        setcookie('birthday_value', $_POST['birthday'], 0);
     }
     else {
-        setcookie('birthday_value', $_POST['birthday'], time() + 30 * 24 * 60 * 60);
+        setcookie('birthday_value', $_POST['birthday'], time() + 365 * 24 * 60 * 60);
         setcookie('birthday_error', '', 100000);
     }
 
     if (empty($_POST['gender'])) {
         setcookie('gender_error', 'empty');
         $errors = TRUE;
+        setcookie('gender_value', $_POST['gender'], 0);
     }
     else {
-        setcookie('gender_value', $_POST['gender'], time() + 30 * 24 * 60 * 60);
+        setcookie('gender_value', $_POST['gender'], time() + 365 * 24 * 60 * 60);
         setcookie('gender_error', '', 100000);
     }
 
     if (empty($_POST['limbs'])) {
         setcookie('limbs_error', 'empty');
         $errors = TRUE;
+        setcookie('limbs_value', $_POST['limbs'], 0);
     }
     else {
-        setcookie('limbs_value', $_POST['limbs'], time() + 30 * 24 * 60 * 60);
+        setcookie('limbs_value', $_POST['limbs'], time() + 365 * 24 * 60 * 60);
         setcookie('limbs_error', '', 100000);
     }
     
     if (empty($_POST['superpowers'])) {
         setcookie('superpowers_error', 'empty');
         $errors = TRUE;
+        setcookie('superpowers_value', $_POST['superpowers'], 0);
     }
     else {
-        setcookie('superpowers_value', implode(', ', $_POST['superpowers']), time() + 30 * 24 * 60 * 60);
+        setcookie('superpowers_value', implode(', ', $_POST['superpowers']), time() + 365 * 24 * 60 * 60);
         setcookie('superpowers_error', '', 100000);
     }
     
     if (empty($_POST['biography'])) {
         setcookie('biography_error', 'empty');
         $errors = TRUE;
+        setcookie('biography_value', $_POST['biography'], 0);
     }
     else {
-        setcookie('biography_value', $_POST['biography'], time() + 30 * 24 * 60 * 60);
+        setcookie('biography_value', $_POST['biography'], time() + 365 * 24 * 60 * 60);
         setcookie('biography_error', '', 100000);
     }      
     
     if (empty($_POST['contract'])) {
         setcookie('contract_error', 'empty');
         $errors = TRUE;
+        setcookie('contract_value', $_POST['contract'], 0);
     }
     else {
-        setcookie('contract_value', $_POST['contract'], time() + 30 * 24 * 60 * 60);
+        setcookie('contract_value', $_POST['contract'], time() + 365 * 24 * 60 * 60);
         setcookie('contract_error', '', 100000);
     }
     
@@ -141,10 +153,10 @@ else {
         $pwrs -> execute(array($usr_id, $superpowers));
     }
     catch(PDOException $e){
-        setcookie('save_error', '$e->getMessage()');
+        setcookie('save_error', '$e->getMessage()', 0);
         header('Location: index.php');
         exit();
     }
-
+    
     header('Location: ?save=1');
 }
