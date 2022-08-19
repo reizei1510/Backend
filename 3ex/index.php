1882ad26e80f23535c118796a53fe8de891afa08
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-19 08:22:10 +0300

    Update index.php

--- 3ex/index.php
+++ 3ex/index.php
@@ -62,9 +62,9 @@ $db = new PDO('mysql:host=localhost;dbname=u16346', $user , $pass, array(PDO::AT
 try {
   $stmt = $db->prepare("INSERT INTO users SET name = ?, email = ?, birthday = ?, gender = ?, limbs = ?, biography = ?");
   $stmt -> execute(array($name, $email, $birthday, $gender, $limbs, $biography));
-  $usr_id = $db->lastInsertId();
-  $pwr = $db->prepare("INSERT INTO powers SET usr_id = ?, superpowers = ?");
-  $pwr -> execute(array($usr_id, $superpowers));
+  //$usr_id = $db->lastInsertId();
+  $pwr = $db->prepare("INSERT INTO powers SET /*usr_id = ?, */superpowers = ?");
+  $pwr -> execute(array(/*$usr_id, */$superpowers));
 }
 catch(PDOException $e){
   print('Error : ' . $e->getMessage());
