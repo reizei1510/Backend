f3a30601715e0f9762cbf55adf6dfc86bc269eee
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-19 07:42:22 +0300

    Update index.php

--- 3ex/index.php
+++ 3ex/index.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (!empty($_GET['save'])) {
-        echo "<script type='text/javascript'>alert('Спасибо, результаты сохранены.');</script>";
+        echo "<script type='text/javascript'>alert('Результаты сохранены.');</script>";
     }
     include('form.php');
     exit();
