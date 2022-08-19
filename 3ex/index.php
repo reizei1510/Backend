06fb8fd6624c2fd4c6101aeb65ee40aba05b93cf
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-19 06:13:07 +0300

    Update index.php

--- 3ex/index.php
+++ 3ex/index.php
@@ -53,7 +53,7 @@ $limbs = $_POST['limbs'];
 $superpowers = implode(',',$_POST['superpowers']); // объединить элементы массива в строку
 $biography = $_POST['biography'];
 
-print($name . $email . $birthday . $gender . $limbs . $superpowers . $biography);
+echo "<script type='text/javascript'>alert($name + $email + $birthday + $gender + $limbs + $superpowers + $biography);</script>";
 
 /*$user = 'u16346';
 $pass = '34rerfeq5';
