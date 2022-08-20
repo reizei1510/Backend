31c24c8e9d33817a82dde7a6896cc4bf7ec80660
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-20 07:50:11 +0300

    Update login.php

--- 5ex/login.php
+++ 5ex/login.php
@@ -11,25 +11,21 @@ if (!empty($_SESSION['login'])) {
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-  $errors = array();
-  $errors['usr_login'] = empty($_COOKIE['usr_login_error']) ? false : $_COOKIE['usr_login_error'];
-  $errors['usr_pass'] = empty($_COOKIE['usr_login_error']) ? false : $_COOKIE['usr_login_error'];
+    $errors = array();
+    $errors['usr_login'] = empty($_COOKIE['usr_login_error']) ? false : $_COOKIE['usr_login_error'];
+    $errors['usr_pass'] = empty($_COOKIE['usr_login_error']) ? false : $_COOKIE['usr_login_error'];
 
-  $messages = array();
-  if ($errors['usr_login']) {
-    setcookie('usr_login_error', '', 100000);
-    $messages['usr_login'] = $errors['usr_login'] == 'empty' ? 'Введите логин.' : 'Данный логин не зарегистрирован.';
-  }
-  else $messages['usr_login'] = '';
-  if ($errors['usr_pass']) {
-    setcookie('usr_pass_error', '', 100000);
-    $messages['usr_pass'] = $errors['usr_pass'] == 'empty' ? 'Введите пароль.' : 'Неверный пароль.';
-  }
-  else $messages['usr_pass'] = '';
-  
-   $values = array();
-   $values['usr_login'] = empty($_COOKIE['usr_login']) ? '' : $_COOKIE['usr_login'];
-   $values['usr_pass'] = empty($_COOKIE['usr_pass']) ? '' : $_COOKIE['usr_pass'];
+    $messages = array();
+    if ($errors['usr_login']) {
+        setcookie('usr_login_error', '', 100000);
+        $messages['usr_login'] = $errors['usr_login'] == 'empty' ? 'Введите логин.' : 'Данный логин не зарегистрирован.';
+    }
+    else $messages['usr_login'] = '';
+    if ($errors['usr_pass']) {
+        setcookie('usr_pass_error', '', 100000);
+        $messages['usr_pass'] = $errors['usr_pass'] == 'empty' ? 'Введите пароль.' : 'Неверный пароль.';
+    }
+    else $messages['usr_pass'] = '';
 
 ?>
 
@@ -52,14 +48,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             <label>
                 Логин:<br>
                 <input name="usr_login" <?php if ($errors['usr_login']) { print 'class="error"'; } ?> /><br>
-		            <div class="error_message"><?php print $messages['usr_login']; ?></div>
+		<div class="error_message"><?php print $messages['usr_login']; ?></div>
             </label><br>
             <label>
                 Пароль:<br>
-                <input name="usr_pass" <?php if ($errors['usr_pass']) { print 'class="error"'; } ?> ?> /><br>
-		            <div class="error_message"><?php print $messages['usr_pass']; ?></div>
+                <input name="usr_pass" type="password" <?php if ($errors['usr_pass']) { print 'class="error"'; } ?> ?> /><br>
+		<div class="error_message"><?php print $messages['usr_pass']; ?></div>
             </label><br>
-                <input type="submit" value="Войти" />
+            <input type="submit" value="Войти" />
         </form>
           
 <?php
@@ -68,49 +64,54 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 else {
   
-  if (empty($_POST['usr_login'])) {
-      setcookie('usr_login_error', 'empty', time() + 24 * 60 * 60);
-      header('Location: login.php');
-      exit();
-  }
+    $errors = FALSE;
+    if (empty($_POST['usr_login'])) {
+        setcookie('usr_login_error', 'empty', time() + 24 * 60 * 60);
+	$errors = TRUE;
+   }
   
-  if (empty($_POST['usr_pass'])) {
-      setcookie('usr_pass_error', 'empty', time() + 24 * 60 * 60);
-      header('Location: login.php');
-      exit();
-  }
-  
-  $usr_login=$_POST['usr_login'];
-  $usr_pass=md5($_POST['usr_pass']);
+   if (empty($_POST['usr_pass'])) {
+        setcookie('usr_pass_error', 'empty', time() + 24 * 60 * 60);
+	$errors = TRUE;
+    }
+	
+    if ($errors) {
+        header('Location: login.php');
+        exit();
+    }
+    
+    $usr_login=$_POST['usr_login'];
+    $usr_pass=$_POST['usr_pass'];
 
-  $db_login = 'u16346';
-  $db_pass = '34rerfeq5';
-  $db = new PDO('mysql:host=localhost;dbname=u16346', $db_login, $db_pass, array(PDO::ATTR_PERSISTENT => true));
+    $db_login = 'u16346';
+    $db_pass = '34rerfeq5';
+    $db = new PDO('mysql:host=localhost;dbname=u16346', $db_login, $db_pass, array(PDO::ATTR_PERSISTENT => true));
 
-  $checkLogin = $db->query("SELECT usr_login FROM users5 WHERE usr_login = $usr_login");
-  foreach($checkLogin as $user)
-      $tempLogin=$user['usr_login'];
-  if (empty($tempLogin)) {
-      setcookie('login_error', 'incorrect', time() + 24 * 60 * 60);
-      header('Location: login.php');
-      exit();
-  }
+    $checkLogin = $db->query("SELECT usr_login FROM users5 WHERE usr_login = $usr_login");
+    foreach($checkLogin as $user)
+        $varLogin=$user['usr_login'];
+    if (empty($varLogin)) {
+        setcookie('login_error', 'incorrect', time() + 24 * 60 * 60);
+        header('Location: login.php');
+        exit();
+    }
   
-  $checkPass = $db->query("SELECT usr_pass FROM users5 WHERE usr_login = $usr_login");
-  foreach($checkPass as $user)
-      $tempPass=$user['usr_pass'];
-  if ($tempPass != $usr_pass){
-      setcookie('pass_error', 'incorrect', time() + 24 * 60 * 60);
-      header('Location: login.php');
-      exit();
-  }
+    $checkPass = $db->query("SELECT usr_pass FROM users5 WHERE usr_login = $usr_login");
+    foreach($checkPass as $user)
+        $varPass=$user['usr_pass'];
+    if ($varPass != $usr_pass){
+        setcookie('pass_error', 'incorrect', time() + 24 * 60 * 60);
+        header('Location: login.php');
+        exit();
+    }
   
-  $_SESSION['usr_login'] = $_POST['usr_login'];
-  $checkUser = $db->query("SELECT id FROM users5 WHERE usr_login=$usr_login AND usr_pass=$usr_pass");
-  foreach($checkUser as $user)
-      $id = (int)$user['id'];
-      $_SESSION['uid'] = $id;
+    $_SESSION['login'] = $_POST['usr_login'];
+    $checkUser = $db->query("SELECT id FROM users5 WHERE usr_login=$usr_login AND usr_pass=$usr_pass");
+    foreach($checkUser as $user)
+        $id = (int)$user['id'];
+    $_SESSION['uid'] = $id;
   }
 
   header('Location: index.php');
+  exit();
 }
