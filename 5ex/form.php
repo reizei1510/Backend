9b4746837fb738cbec7e99fdc47b5bb445cf436d
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-20 12:19:48 +0300

    Update form.php

--- 5ex/form.php
+++ 5ex/form.php
@@ -23,34 +23,29 @@
     <div class="content">
 	    
 	<div class="point data_message"><?php print $messages['data']; ?></div><br>
-	    
-        <form action="" method="POST">
+	
+	<div class="point">
 		
-	    <div class="point">
+            <form action="" method="POST">
+	    
                 <label>
                     Имя:<font color="red">*</font><br>
                     <input name="name" <?php if ($errors['name']) { print 'class="error"'; } ?> value="<?php print $values['name']; ?>" /><br>
 		    <div class="error_message"><?php print $messages['name']; ?></div>
-            	</label>
-	    </div><br>
-		
-	    <div class="point">
+            	</label><br>
+	
                 <label>
                     e-mail:<font color="red">*</font><br>
                     <input name="email" <?php if ($errors['email']) { print 'class="error"'; } ?> value="<?php print $values['email']; ?>" /><br>
 		    <div class="error_message"><?php print $messages['email']; ?></div>
-                </label>
-	    </div><br>
+                </label><br>
 		
-	    <div class="point">
                 <label>
                     Дата рождения:<font color="red">*</font><br>
                     <input name="birthday" type="date" <?php if ($errors['birthday']) { print 'class="error"'; } ?> value="<?php print $values['birthday']; ?>" /><br>
 		    <div class="error_message"><?php print $messages['birthday']; ?></div>
-                </label>
-	    </div><br>
+                </label><br>
 	    
-	    <div class="point">
                 Пол:<font color="red">*</font><br>
                 <label>
                     <input type="radio" name="gender" value="Male" <?php if($values['gender'] == "Male"){ print "checked='checked'"; } ?> />Мужской
@@ -61,10 +56,8 @@
                 <label>
                     <input type="radio" name="gender" value="other" <?php if($values['gender'] == "other"){ print "checked='checked'"; } ?> />Другое
                 </label><br>
-		<div class="error_message"><?php print $messages['gender']; ?></div>
-	    </div><br>
+		<div class="error_message"><?php print $messages['gender']; ?></div><br>
 		
-	    <div class="point">
                 Количество конечностей:<font color="red">*</font><br>
                 <label>
                     <input type="radio" name="limbs" value="2" <?php if($values['limbs'] == "2"){ print "checked='checked'"; } ?> />2
@@ -75,10 +68,8 @@
                 <label>
                     <input type="radio" name="limbs" value="other" <?php if($values['limbs'] == "other"){ print "checked='checked'"; } ?> />Другое
                 </label><br>
-		<div class="error_message"><?php print $messages['limbs']; ?></div>
-	    </div><br>
+		<div class="error_message"><?php print $messages['limbs']; ?></div><br>
       
-	    <div class="point">
                 <label>
                     Сверхспособность:<font color="red">*</font><br>
                     <select name="superpowers[]" multiple="multiple" <?php if ($errors['superpowers']) { print 'class="error"'; } ?> >
@@ -87,31 +78,26 @@
                         <option value="Levitation" <?php if (in_array("Levitation", $values['superpowers'])) { print "selected='selected'";} ?>>Левитация</option>
                     </select>
                 </label><br>
-		<div class="error_message"><?php print $messages['superpowers']; ?></div>
-	    </div><br>
+		<div class="error_message"><?php print $messages['superpowers']; ?></div><br>
 		
-	    <div class="point">
                 <label>
                     Биография:<font color="red">*</font><br>
                     <textarea name="biography" <?php if ($errors['biography']) { print 'class="error"'; } ?>><?php print $values['biography']; ?></textarea>
                 </label><br>
-		<div class="error_message"><?php print $messages['biography']; ?></div>
-	    </div><br>
+		<div class="error_message"><?php print $messages['biography']; ?></div><br>
 		
-	    <div class="point">
                 <label>
                     <input type="checkbox" name="contract" <?php if ($values['contract']) { print "checked='checked'"; } ?> />С контрактом ознакомлен(а)<font color="red">*</font>
                 </label><br>
-		<div class="error_message"><?php print $messages['contract']; ?></div>
-	    </div><br>
+		<div class="error_message"><?php print $messages['contract']; ?></div><br>
 		
-	    <div class="point">
                 <input class="button" type="submit" value="Отправить" /><br>
 		<div class="error_message"><?php print $messages['save']; ?></div>
-	    </div>
+	
+	    </form>    
 		
-        </form>
-          
+	</div>
+        
     </div>
  
 </body>
