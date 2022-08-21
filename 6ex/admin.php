edf5cc42d51fcc2956baf4b2dd573d21f05cf0c3
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-21 08:14:22 +0300

    Update admin.php

--- 6ex/admin.php
+++ 6ex/admin.php
@@ -20,6 +20,94 @@ if (empty($admin) || $admin['adm_pass'] != $_SERVER['PHP_AUTH_PW']) {
     exit();
 }
 
+
+
+
+
+
+
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['delete'])) {
+        $del_user = $db->prepare("DELETE FROM users6 WHERE usr_id = ?");
+        $del_user->execute(array($_POST['delete']));
+        $del_powers = $db->prepare("DELETE FROM powers6 WHERE usr_id = ?");
+        $del_powers->execute(array($_POST['delete']));
+        $del_data = $db->prepare("DELETE FROM users_data6 WHERE usr_id = ?");
+        $del_data->execute(array($_POST['delete']));
+    }
+   
+    /*else if (!empty($_POST['edit'])) {
+        $stmt = $db->prepare("SELECT * FROM members WHERE login = ?");
+        $stmt->execute(array($member_id));
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $values['name'] = $result['name'];
+        $values['email'] = $result['email'];
+        $values['birth'] = $result['date'];
+        $values['gender'] = $result['gender'];
+        $values['limbs'] = $result['limbs'];
+        $values['bio'] = $result['bio'];
+        $values['policy'] = $result['policy'];
+
+        setcookie('user_id', $member_id, time() + 12 * 30 * 24 * 60 * 60);
+
+        $powers = $db->prepare("SELECT * FROM powers2 WHERE user_login = ?");
+        $powers->execute(array($member_id['login']));
+        $result = $powers->fetch(PDO::FETCH_ASSOC);
+        $values['select'] = $result['powers'];
+    }
+    else {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $date = $_POST['birth'];
+        $gender = $_POST['gender'];
+        $limbs = $_POST['limbs'];
+        $bio = $_POST['bio'];
+        $policy = $_POST['policy'];
+        $select = implode(',', $_POST['select']);
+        $user = 'u47572';
+        $pass = '4532025';
+        $db = new PDO('mysql:host=localhost;dbname=u47572', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+        $member_id = $_COOKIE['user_id'];
+
+        try {
+            $stmt = $db->prepare("SELECT login FROM members WHERE id = ?");
+            $stmt->execute(array($member_id));
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            setcookie('login_value', $result['login'], time() + 12 * 30 * 24 * 60 * 60);
+
+            $stmt = $db->prepare("UPDATE members SET name = ?, email = ?, date = ?, gender = ?, limbs = ?, bio = ?, policy = ? WHERE login = ?");
+            $stmt->execute(array($name, $email, $date, $gender, $limbs, $bio, $policy, $result['login']));
+
+            $superpowers = $db->prepare("UPDATE powers2 SET powers = ? WHERE user_login = ? ");
+            $superpowers->execute(array($select, $result['login']));
+        } catch (PDOException $e) {
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $stmt = $db->query("SELECT * FROM users6");
     $allUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
