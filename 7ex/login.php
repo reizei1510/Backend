08a12f88d6f7e66a1bf6876818e278313ae3cb8c
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-21 10:28:31 +0300

    Create login.php

---
+++ 7ex/login.php
@@ -0,0 +1,114 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+	
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	
+    if (!empty($_SESSION['login'])) {
+        session_destroy();
+        setcookie('name_value', '', 100000);
+        setcookie('email_value', '', 100000);
+        setcookie('birthday_value', '', 100000);
+        setcookie('gender_value', '', 100000);
+        setcookie('limbs_value', '', 100000);
+        setcookie('superpowers_value', '', 100000);
+        setcookie('biography_value', '', 100000);
+        setcookie('contract_value', '', 100000);
+        header('Location: ./');
+    }
+
+    $errors = array();
+    $errors['usr_login'] = empty($_COOKIE['usr_login_error']) ? false : $_COOKIE['usr_login_error'];
+    $errors['usr_pass'] = empty($_COOKIE['usr_pass_error']) ? false : $_COOKIE['usr_pass_error'];
+
+    $messages = array();
+    if ($errors['usr_login']) {
+        setcookie('usr_login_error', '', 100000);
+        $messages['usr_login'] = $errors['usr_login'] == 'empty' ? 'Введите логин.' : 'Данный логин не зарегистрирован.';
+    }
+    else $messages['usr_login'] = '';
+    if ($errors['usr_pass']) {
+        setcookie('usr_pass_error', '', 100000);
+        $messages['usr_pass'] = $errors['usr_pass'] == 'empty' ? 'Введите пароль.' : 'Неверный пароль.';
+    }
+    else $messages['usr_pass'] = '';
+
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Вход</title>
+</head>
+
+<body>
+    <div class="topnav">
+        <a href="index.php">Задание 5</a>
+    </div>
+ 
+    <div class="content">
+	    
+        <form action="" method="POST">
+  
+            <label>
+                Логин:<br>
+                <input name="usr_login" <?php if ($errors['usr_login']) { print 'class="error"'; } ?> /><br>
+		<div class="error_message"><?php print $messages['usr_login']; ?></div>
+            </label><br>
+            <label>
+                Пароль:<br>
+                <input name="usr_pass" type="password" <?php if ($errors['usr_pass']) { print 'class="error"'; } ?> /><br>
+		<div class="error_message"><?php print $messages['usr_pass']; ?></div>
+            </label><br>
+            <input type="submit" class="button" value="Войти" />
+        </form>
+          
+<?php
+  
+}
+
+else {
+  
+    $errors = FALSE;
+    if (empty($_POST['usr_login'])) {
+        setcookie('usr_login_error', 'empty', time() + 24 * 60 * 60);
+	$errors = TRUE;
+   }
+  
+   if (empty($_POST['usr_pass'])) {
+        setcookie('usr_pass_error', 'empty', time() + 24 * 60 * 60);
+	$errors = TRUE;
+    }
+	
+    if ($errors) {
+        header('Location: login.php');
+        exit();
+    }
+
+    $db_login = 'u16346';
+    $db_pass = '34rerfeq5';
+    $db = new PDO('mysql:host=localhost;dbname=u16346', $db_login, $db_pass, array(PDO::ATTR_PERSISTENT => true));
+	
+    $stmt = $db->prepare("SELECT * FROM users_data6 WHERE usr_login = ?");
+    $stmt->execute([$_POST['usr_login']]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+	
+    if (empty($user)) {
+        setcookie('usr_login_error', 'incorrect', time() + 24 * 60 * 60);
+        header('Location: login.php');
+        exit();
+    }
+    if ($user['usr_pass'] != $_POST['usr_pass']) {
+        setcookie('usr_pass_error', 'incorrect', time() + 24 * 60 * 60);
+        header('Location: login.php');
+        exit();
+    }
+  
+    $_SESSION['login'] = $_POST['usr_login'];
+    $_SESSION['uid'] = $user['usr_id'];	
+
+    header('Location: ./');
+}
