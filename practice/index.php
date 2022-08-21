a40df439d452b8f63d41809fca0d13bee5ac2042
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 00:56:54 +0300

    Update index.php

--- practice/index.php
+++ practice/index.php
@@ -14,19 +14,22 @@
     <div class="content">
         <div class="welcome"><h1>Diary</h1></div>
 	<div class="welcome_text">Welcome</div>
-	<p>You can <a href="logup.php">Join</a> or <a href="login.php">Login</a> or <a href="read.php">Read</a>
-      
+    </div>
+    <div class="content">
+	You can <a href="logup.php">Join</a> or <a href="login.php">Login</a> or <a href="read.php">Read</a>
     </div>
 	
     <footer>
         <table>
 	    <tr>
                 <td><a href="about.php">About</a></td>
-                <td><a href="contacts.php">Contacts</a></td>
-                <td><a href="contacts.php">Rules</a></td>
+                <td><a href="admin.php">Are you admin?</a></td>
 	    </tr>
             <tr>
-                <td><a href="admin.php">Are you admin?</a></td>
+                <td><a href="contacts.php">Contacts</a></td>
+            </tr>
+            <tr>
+                <td><a href="contacts.php">Rules</a></td>
             </tr>
         </table>
     <footer>
