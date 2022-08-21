4f93fbaabd10affa9c668d9b399c5b6fefe644c9
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-21 08:03:36 +0300

    Update admin.php

--- 6ex/admin.php
+++ 6ex/admin.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 <body>
 	
-    <div class="content">Успешная авторизация. <a href="./admin.php?logout=1">Выйти</a></div>
+    <div class="content">Успешная авторизация.<br><a href="./admin.php?logout=1">Выйти</a></div>
     
     <div class="content">
 	    
