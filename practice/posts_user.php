fde2b633846bbfd157c053ad42ec70092597d235
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 22:45:42 +0300

    Create posts_user.php

---
+++ practice/posts_user.php
@@ -0,0 +1,79 @@
+<?php
+$stmt = $db->prepare("SELECT * FROM posts WHERE usr_id = ?");
+$stmt->execute([$pu]);
+$psts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./styles.css" />
+    <title>Admin</title>
+</head>
+
+<body>
+	
+  <div class="page">
+    
+    <div class="topnav">
+	  <a href="index.php">Diary</a>
+	  <div class="topnav_right">
+		<?php
+		if (session_start() && empty($_SESSION['login'])) {
+	      print '<a href="login.php">Log In</a>';  
+		    print '<a href="logup.php">Log Up</a>';
+		}
+		else {
+		    print '<a href="profile.php">Profile</a>'; 
+		}
+		?>
+	  </div>
+    </div>
+    
+    <div class="content">
+      
+      <div class="admin">
+	    
+        <table>
+            <tr>
+                <th>User ID</th>
+                <th>Note ID</th>
+                <th>Note</th>
+                <th colspan="2">Acts</th>
+            </tr>
+            <?php
+            if (!empty($psts)) {
+                foreach ($psts as $p) {
+            ?>
+                    <tr>
+                        <td><?php print $p['usr_id'] ?></td>
+                        <td><?php print $p['post_id'] ?></td>
+                        <td><?php print $p['post'] ?></td>
+                      
+                        <td><form action="" method="POST">
+                            <input value="<?php print $p['post_id'] ?>" name="edit_p" type="hidden" /><button id="edit_p">Edit</button>
+                            </form></td>
+                        <td><form action="" method="POST">
+                            <input value="<?php echo $usr['post_id'] ?>" name="delete_p" type="hidden" /><button id="delete_p">Delete</button>
+                            </form></td>
+                    </tr>
+            <?php
+                }
+            } else {
+                print '<tr><td colspan="5">Записи не найдены</td></tr>';
+            }
+            ?>
+        </table>
+        
+      </div>
+	    
+    </div>
+    
+  </div>
+	
+</body>
+
+</html>
