7190d5fe8cf4288f0cde3fd9b6df4b2e7a966e02
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 05:38:49 +0300

    Create about.php

---
+++ practice/about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>About</title>
+</head>
+
+<body>
+    <div class="topnav">
+        <a href="index.php">Diary</a>
+        <div class="topnav_right">
+            <?php
+            if (empty($_SESSION['login'])) {
+                print '<a href="login.php">Log In</a>';  
+                print '<a href="logup.php">Log Up</a>';
+            }
+            else {
+                print '<a href="login.php"><img src="profile.png" id="profile" alt="profile"></a>'; 
+            }
+            ?>
+        </div>
+    </div>
+  
+        <div class="page">
+      
+            <div class="content">
+                <div class="description">
+                    This is a small website for summer internships at the university.
+                </div>
+            </div>
+    	    
+        </div>
+	
+    <footer>
+        <table>
+	    <tr>
+                <td><a href="about.php">About</a></td>
+                <td><a href="contacts.php">Contacts</a></td>
+                <td><a href="contacts.php">Rules</a></td>
+	    </tr>
+            <tr>
+                <td><a href="admin.php">Are you admin?</a></td>
+            </tr>
+        </table>
+    <footer>
+
+</body>
