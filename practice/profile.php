e6a1d72db451a5e6312969ccfcbeda76772f3d1a
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 19:10:47 +0300

    Update profile.php

--- practice/profile.php
+++ practice/profile.php
@@ -55,8 +55,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
     else if (!empty($_POST['update_info'])) {
         try {
-            $stmt = $db->prepare("UPDATE diary_users SET usr_login = ?, gender = ?, bio = ?");
-            $stmt->execute(array($_POST['name'], $_POST['gender'], $_POST['bio']));
+            $stmt = $db->prepare("UPDATE diary_users SET usr_login = ?, usr_pass = ?, gender = ?, bio = ?");
+            $stmt->execute(array($_POST['name'], $_POST['pass'], $_POST['gender'], $_POST['bio']));
 	    header('Location: ./profile.php');
         }
 	catch (PDOException $e) {
