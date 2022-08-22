873580c7db4d1c48d271de4531a4a132a88a0a2c
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-23 00:30:23 +0300

    Create read.php

---
+++ practice/read.php
@@ -0,0 +1,78 @@
+<?php
+$db_login = 'u16346';
+$db_pass = '34rerfeq5';
+$db = new PDO('mysql:host=localhost;dbname=u16346', $db_login, $db_pass, array(PDO::ATTR_PERSISTENT => true));
+
+$stmt=$db->prepare("SELECT * FROM posts");
+$allposts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Read</title>
+</head>
+
+<body>
+    <div class="page">
+	<div class="topnav">
+	    <a href="index.php">Diary</a>
+	    <div class="topnav_right">
+		<?php
+		if (session_start() && empty($_SESSION['login'])) {
+	            print '<a href="login.php">Log In</a>';  
+		    print '<a href="logup.php">Log Up</a>';
+		}
+		else {
+		    print '<a href="profile.php">Profile</a>'; 
+		}
+		?>
+	    </div>
+	</div>
+        <div class="content">
+            <div class="description">
+                Here you can read other users.
+            </div>
+            <?php
+            if (empty($allposts)) {
+                print '<div class="description">No any notes.</div>';
+            }
+            else {
+                $stmt=$db->prepare("SELECT user_id FROM posts WHERE posts_id = ?");
+                $stmt->execute([$post['post_id']]);
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                $stmt=$db->prepare("SELECT user_login FROM diary_users WHERE usr_id = ?");
+                $stmt->execute([$user['usr_id']]);
+                $autor = $stmt->fetch(PDO::FETCH_ASSOC);
+                foreach ($allposts as $post) {
+                    print '<div class="description">';
+                    print $autor['usr_login'];
+                    print $post['date'];
+                    if ($post['up_date'] > $post['date']) {
+                        print '<br>updated';
+                        print $post['up_date'];
+                    }
+                    print '<br><br>';
+                    print $post['post'];
+                    print '<br><br>'; ?>
+                </div>
+	      <?php
+                }
+            }
+            ?>
+        </div>
+    	    
+	
+	<footer>
+	    <a href="about.php">About</a>
+	    <a href="contacts.php">Contacts</a>
+	    <a href="rules.php">Rules</a>
+	    <a href="admin.php">Are you admin?</a>
+	<footer>
+		    
+    </div>
+
+</body>
