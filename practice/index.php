834f86bad2bab9f50a894f047ccd94bd59c07b2d
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 04:45:19 +0300

    Update index.php

--- practice/index.php
+++ practice/index.php
@@ -24,18 +24,10 @@
     </div>
 
 	    <footer>
-		<table>
-		    <tr>
-			<td><a href="about.php">About</a></td>
-			<td><a href="admin.php">Are you admin?</a></td>
-		    </tr>
-		    <tr>
-			<td><a href="contacts.php">Contacts</a></td>
-		    </tr>
-		    <tr>
-			<td><a href="contacts.php">Rules</a></td>
-		    </tr>
-		</table>
+		<a href="about.php">About</a>
+		<a href="contacts.php">Contacts</a>
+		<a href="contacts.php">Rules</a>
+		<a href="admin.php">Are you admin?</a>
 	    <footer>
 		    
 </body>
