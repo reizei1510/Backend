44f7872e446347675cba00c18a1b12dfa22b5cd9
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-23 00:19:23 +0300

    Update posts_user.php

--- practice/posts_user.php
+++ practice/posts_user.php
@@ -6,10 +6,6 @@ $db = new PDO('mysql:host=localhost;dbname=u16346', $db_login, $db_pass, array(P
         $stmt->execute([$_POST['posts_user']]);
         $psts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-?>
-
 <!DOCTYPE html>
 <html lang="">
 
@@ -49,7 +45,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             <tr>
                 <th>Note ID</th>
                 <th>Note</th>
-                <th colspan="2">Acts</th>
             </tr>
             <?php
             if (!empty($psts)) {
@@ -58,18 +53,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     <tr>
                         <td><?php print $p['post_id'] ?></td>
                         <td><?php print $p['post'] ?></td>
-                      
-                        <td><form action="" method="POST">
-                            <input value="<?php print $p['post_id'] ?>" name="edit_p" type="hidden" /><button id="edit_p">Edit</button>
-                            </form></td>
-                        <td><form action="" method="POST">
-                            <input value="<?php echo $usr['post_id'] ?>" name="delete_p" type="hidden" /><button id="delete_p">Delete</button>
-                            </form></td>
                     </tr>
             <?php
                 }
             } else {
-                print '<tr><td colspan="5">Записи не найдены</td></tr>';
+                print '<tr><td colspan="2">Записи не найдены</td></tr>';
             }
             ?>
         </table>
@@ -80,14 +68,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	
 </body>
 
-</html>
-
-<?php
-} else {
-        if (!empty($_POST['delete_user'])) {
-          $stmt = $db->prepare("DELETE FROM posts WHERE post_id = ?");
-          $del_posts->execute([$usr['post_id']]);
-	  header('Location: ./posts_user.php?posts_user=<?php print $_POST["posts_user"]; ?>');
-        }
-}
-?>	
+</html>	
