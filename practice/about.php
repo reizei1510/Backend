8b27bced99fdcee14d5d73028c80fd5252cefd8c
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 06:10:30 +0300

    Update about.php

--- practice/about.php
+++ practice/about.php
@@ -8,43 +8,43 @@
 </head>
 
 <body>
-    <div class="topnav">
-        <a href="index.php">Diary</a>
-        <div class="topnav_right">
-            <?php
-            if (empty($_SESSION['login'])) {
-                print '<a href="login.php">Log In</a>';  
-                print '<a href="logup.php">Log Up</a>';
-            }
-            else {
-                print '<a href="login.php"><img src="profile.png" id="profile" alt="profile"></a>'; 
-            }
-            ?>
-        </div>
-    </div>
+    <div class="page">
+	<div class="topnav">
+	    <a href="index.php">Diary</a>
+	    <div class="topnav_right">
+		<?php
+		if (empty($_SESSION['login'])) {
+	            print '<a href="login.php">Log In</a>';  
+		    print '<a href="logup.php">Log Up</a>';
+		}
+		else {
+		    print '<a href="login.php"><img src="profile.png" id="profile" alt="profile"></a>'; 
+		}
+		?>
+	    </div>
+	</div>
   
-        <div class="page">
       
-            <div class="content">
-                <div class="description">
-                    This is a small website for summer internships at the university.
-                </div>
+        <div class="content">
+            <div class="description">
+                This is a small website for summer internships at the university.
             </div>
+        </div>
     	    
 	
-	    <footer>
-		<table>
-		    <tr>
-			<td><a href="about.php">About</a></td>
-			<td><a href="contacts.php">Contacts</a></td>
-			<td><a href="contacts.php">Rules</a></td>
-		    </tr>
-		    <tr>
-			<td><a href="admin.php">Are you admin?</a></td>
-		    </tr>
-		</table>
-	    <footer>
+	<footer>
+	    <table>
+		<tr>
+		    <td><a href="about.php">About</a></td>
+		    <td><a href="contacts.php">Contacts</a></td>
+		    <td><a href="contacts.php">Rules</a></td>
+		</tr>
+		<tr>
+		    <td><a href="admin.php">Are you admin?</a></td>
+		</tr>
+	    </table>
+	<footer>
 		    
-        </div>
+    </div>
 
 </body>
