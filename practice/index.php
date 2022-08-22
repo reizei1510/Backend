88e7d429e9de33dd8ac01335e8abc2462fb7de86
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-22 21:02:11 +0300

    Update index.php

--- practice/index.php
+++ practice/index.php
@@ -14,6 +14,7 @@ if (!empty($_SESSION['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/logo.png" type="image/png">
     <title>Main Page</title>
 </head>
 
