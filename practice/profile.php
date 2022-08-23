247bf94dfc780f79e86fd776a4fe403d8d130b5f
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-23 10:29:10 +0300

    Update profile.php

--- practice/profile.php
+++ practice/profile.php
@@ -91,6 +91,7 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/logo.png" type="image/png">
     <title>Profile</title>
 </head>
 
