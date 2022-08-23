1006c6e2f8815f4e5e5a70d54a95f41468a15b70
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-23 10:30:25 +0300

    Update posts_by_user.php

--- practice/posts_by_user.php
+++ practice/posts_by_user.php
@@ -46,6 +46,7 @@ else {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
     <link rel="stylesheet" href="styles.css" />
+    <link rel="icon" href="img/logo.png" type="image/png">
     <title>All Posts</title>
 </head>
 
