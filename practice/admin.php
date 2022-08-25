63c98f44efb6945723558c3047189d18a4f078a8
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-25 07:06:30 +0300

    Update admin.php

--- practice/admin.php
+++ practice/admin.php
@@ -41,6 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $values['login'] = $result['usr_login'];
         $values['gender'] = $result['gender'];
         $values['bio'] = $result['bio'];
+	$values['id'] = $_POST['edit_user'];
 
         setcookie('user_id', $_POST['edit_user'], time() + 12 * 30 * 24 * 60 * 60);
         include('edit_user.php');
