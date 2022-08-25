12d9072f09c01e686db3fad2c91968369bf52040
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-08-25 07:24:15 +0300

    Update logout.php

--- practice/logout.php
+++ practice/logout.php
@@ -1,6 +1,7 @@
 <?php
 if (session_start() && !empty($_SESSION['login'])) {
-    setcookie('session_id', $_SESSION['id'], 1000000);
+    setcookie('session_id', '', 1000000);
+    setcookie('session_login', '', 1000000);
     session_destroy();
 }
 header('Location: ./');
